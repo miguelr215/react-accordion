eee53634b268c5857ef2a7782e58e4905114e9f7
miguelr215 <drexelramos@gmail.com>
2024-02-18 07:08:00 -0500

    accordion state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const faqs = [
+  {
+    title: 'What shirt sizes do you offer?',
+    text: 'Our shirts range from XS to XXXL for both men and women.',
+  },
+  {
+    title: 'What is your return policy?',
+    text: 'Returns must be initiated within 30 days of package delivery.',
+  },
+  {
+    title: 'What shirt colors do you offer?',
+    text: 'Most shirts are available in various colors ranging from red, black, blue, yellow, green, and white.',
+  },
+];
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  return (
+    <div className="accordion-list">
+      {data.map((item, i) => (
+        <AccordionItem
+          key={item.title}
+          title={item.title}
+          text={item.text}
+          num={i + 1}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`accordion-item ${isOpen ? 'open' : ''}`}>
+      <div className="accordion-header">
+        <span className="number">{num}</span>
+        <p className="title">{title}</p>
+        <button
+          type="button"
+          className={`${isOpen ? 'open' : ''}`}
+          onClick={handleToggle}
+        >
+          <span class="material-symbols-outlined">arrow_drop_down</span>
+        </button>
+      </div>
+      <p className={`accordion-answer ${isOpen ? 'show' : ''}`}>{text}</p>
+    </div>
+  );
+}
